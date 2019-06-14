a69aebd4598eb52a15327fa2696a63125cc0c9b9
unknown <forison6205@gmail.com>
2019-06-14 16:35:17 +0000

    test refactory

--- spec/custom_enum_spec.rb
+++ spec/custom_enum_spec.rb
@@ -4,33 +4,35 @@ RSpec.describe Enumerable do
     
     let(:test_array_one) { [1, 2, 3, 4, 5] }
     let(:test_array_two) { ['a','a','a','b'] }
+    let(:test_array_three) { ['kofi','ama', 99] }
+    let(:test_array_four) { ['kofi','ama', 'adwoa'] }
+    let(:test_array_five) { ['kofi','akosua', 'adwoa'] }
+    let(:empty_array_test) { [ ] }
+    let(:test_array_bool_1) { [false, false, true] }
+    let(:hash_test){ { 1 => 'hi', 2 => 'hello', 3 => 'how'} }
 
     describe "#my_each" do
       it " It returns each element in array when given a block " do
-        
-        expect( test_array_one.my_each { |a|  a } ).to eql([1,2,3,4,5])
+        expect( test_array_one.my_each { |a|  a } ).to eql(test_array_one)
       end
       
       it " It returns each key or value in hash when given a block with two parameter " do
-        hash={ 1 => 'hi', 2 => 'hello', 3 => 'how'}
-        expect( hash.my_each { |a,b|  b } ).to eql( {1=>"hi", 2=>"hello", 3=>"how"} )
+        hash=
+        expect( hash_test.my_each { |a,b|  b } ).to eql( hash_test )
       end
 
     end
 
     describe " #my_count(para = nil) " do 
       it "It returns the number of elements in array when no parameter is given and no block is passed " do
-       
          expect( test_array_one.my_count ).to eql( 5 )
       end
 
       it "It returns the number of the argument that exist in the array it operates upon,when no block is given " do
-        
          expect( test_array_two.my_count('a') ).to eql( 3 )
       end
        
       it "It returns the number of that exist in the array based on the condition specified in the block " do
-        
          expect( test_array_two.my_count { |a| a  =='b' } ).to eql( 1 )
       end
       
@@ -38,27 +40,27 @@ RSpec.describe Enumerable do
 
     describe "#my_any?(arg = nil)" do
        it "It return false if array is empty and neither a block or agument is passed" do
-        array = []
-        expect( array.my_any? ).to eql(false)
+        
+        expect( empty_array_test.my_any? ).to eql(false)
        end
        it "It return true if any element in the array is true and neither a block or argument is given" do
-        array = [false, false, true]
-        expect( array.my_any? ).to eql(true)
+        
+        expect( test_array_bool_1.my_any? ).to eql(true)
        end
        it "It return true if any element in the array is of the same type as the argument passed " do
-        array = ['kofi','ama', 99]                     
-        expect( array.my_any?( Integer ) ).to eql(true)
+                             
+        expect( test_array_three.my_any?( Integer ) ).to eql(true)
        end
 
        it "It return true based on the codition specified in the block when give a block" do
-        array = ['kofi','ama', 'adwoa']                     
-        expect( array.my_any? { |word| word.length >= 3 }  ).to eql(true)
+                             
+        expect( test_array_four.my_any? { |word| word.length >= 3 }  ).to eql(true)
        end
 
        
        it "It return false if none of the above condition is not true" do
-        array = ['kofi','akosua', 'adwoa']                     
-        expect( array.my_any? { |word| word.length == 3 }  ).to eql(false)
+                             
+        expect( test_array_five.my_any? { |word| word.length == 3 }  ).to eql(false)
        end
 
     end
@@ -89,12 +91,8 @@ RSpec.describe Enumerable do
 
         
         it " It returns the key and the value of each element in an array by passing two arguments to the block " do
-            h = {
-                1 =>'kofi',
-                2 => 'ama',
-                3 => 'adwoa'
-            }
-            expect( h.my_map { |e ,f| f } ).to eql(  {1=>"kofi", 2=>"ama", 3=>"adwoa"} )
+           
+            expect( hash_test.my_map { |e ,f| f } ).to eql(  hash_test )
         end
 
     end
